e6bae4659863f8a158b2e6c4c37e3e6704e6972b
nodi-official <mukumov.nodir1@gmail.com>
2021-04-25 19:10:40 +0500

    Solution to Lab 8

---
+++ resources/views/blog/post.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts/master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">Learning Laravel</p>
+        </div>
+    </div>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at vulputate leo. Praesent lacus nisl, finibus sit amet iaculis et, porta eget enim.
+        Etiam vulputate semper felis quis ultrices. </p>
+
+@endsection
+
---
+++ resources/views/other/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts/master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <p class="quote">About Me</p>
+        </div>
+    </div>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at vulputate leo. Praesent lacus nisl, finibus sit amet iaculis et, porta eget enim.
+        Etiam vulputate semper felis quis ultrices. </p>
+
+@endsection
